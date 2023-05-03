8c035b981a1920798b3064073dddee131989f391
ryhnpngst <rayhanwibowo24@gmail.com>
2023-05-03 13:13:45 +0700

    Add
    Add Hamburger Menu, fix layout

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,21 @@
 import "regenerator-runtime"; /* for async await transpile */
 import "../styles/main.css";
 import "./sw.js";
+
+const menu = document.querySelector("#menu");
+const hero = document.querySelector(".hero");
+const main = document.querySelector("main");
+const drawer = document.querySelector("#drawer");
+
+menu.addEventListener("click", (event) => {
+  drawer.classList.toggle("open");
+  event.stopPropagation();
+});
+
+hero.addEventListener("click", () => {
+  drawer.classList.remove("open");
+});
+
+main.addEventListener("click", () => {
+  drawer.classList.remove("open");
+});
