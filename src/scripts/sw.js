53f45c80b6f5f2fa6463714de5a89568a7174f02
ryhnpngst <rayhanwibowo24@gmail.com>
2023-05-03 15:11:01 +0700

    Fix Aksesibilitas

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -4,7 +4,7 @@ const container = document.querySelector(".posts");
 data.restaurants.forEach((restaurant) => {
   container.innerHTML += `
         <div class="post-item">
-        <img class="post-item__thumbnail" src="${restaurant.pictureId}" alt="">
+        <img class="post-item__thumbnail" src="${restaurant.pictureId}" alt="${restaurant.name}">
         <div class="post-item__content">
             <p class="post-item__rating">Rating: ${restaurant.rating}</p>
             <p class="post-item__city">Kota ${restaurant.city}</p>
