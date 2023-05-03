e51b17aeb578dca93b4201b7732e7e93b06c3ac3
ryhnpngst <rayhanwibowo24@gmail.com>
2023-05-03 11:21:03 +0700

    Initial commit

---
+++ src/scripts/sw.js
@@ -0,0 +1,16 @@
+import data from "../DATA.json";
+const container = document.querySelector(".posts");
+
+data.restaurants.forEach((restaurant) => {
+  container.innerHTML += `
+        <div class="post-item">
+        <img class="post-item__thumbnail" src="${restaurant.pictureId}" alt="">
+        <div class="post-item__content">
+            <p class="post-item__rating">Rating: ${restaurant.rating}</p>
+            <p class="post-item__city">Kota ${restaurant.city}</p>
+            <h3 class="post-item__title"><a href="#">${restaurant.name}</a></h3>
+            <p class="post-item__description">${restaurant.description}</p>
+        </div>
+        </div>
+    `;
+});
